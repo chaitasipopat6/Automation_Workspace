9b13e806fe446c520e0ad0941ad798416bb17347
chaitasipopat6 <126541290+chaitasipopat6@users.noreply.github.com>
2023-03-15 19:39:15 +0530

    Add files via upload

---
+++ Areaoftriagle.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+public  class Areaoftriagle
+{
+	public static void main(String[] args) 
+	{
+		
+		double a= 0.5;
+		double b = 30;
+		double c= 10;
+		
+		
+		//Arithmetic Operators
+		
+		// +, - ,*, / , %(Modulus)
+		
+		double Areaoftriagle = a * b * c;
+		
+	
+		
+		
+		System.out.println(Areaoftriagle);
+		
+		
+	}
+}
